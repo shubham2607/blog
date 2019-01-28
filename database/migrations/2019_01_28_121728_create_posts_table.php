bcbde08d7d76c02f201745240464db501ed62426
Shubham Mishra <ssshubham.mishra@gmail.com>
2019-01-28 18:43:22 +0530

    Database created and connected

--- database/migrations/2019_01_28_121728_create_posts_table.php
+++ database/migrations/2019_01_28_121728_create_posts_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration {
+class CreatePostsTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -12,13 +12,11 @@ class CreateUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('users', function(Blueprint $table)
+		Schema::create('posts', function(Blueprint $table)
 		{
 			$table->increments('id');
-			$table->string('name');
-			$table->string('email')->unique();
-			$table->string('password', 60);
-			$table->rememberToken();
+			$table->string('title');
+			$table->text('body');
 			$table->timestamp('created_at')->nullable();
 			$table->timestamp('updated_at')->nullable();
 		});
@@ -31,7 +29,7 @@ class CreateUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('users');
+		Schema::drop('posts');
 	}
 
 }
---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,3 @@
+<hr>
+<p class="text-center">Copyright Billfree - All Rights Reserved</p>
+
